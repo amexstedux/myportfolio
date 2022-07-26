cd04c33d30e3eb091caa9a339be3d59b6b6ac2b5
amexstedux <100746766+amexstedux@users.noreply.github.com>
2022-07-26 16:03:43 -0500

    f4
    f4

---
+++ myscripts.js
@@ -0,0 +1,24 @@
+@media screen and (max-width: 600px) {
+    .column {
+      width: 100%;
+    }
+  }
+  @media (min-width: 320px) {
+    .container {
+      padding: 1%;
+    }
+  }
+  
+  
+  @media (min-width: 481px) {
+    .container {
+      padding: 1%;
+    }
+  }
+  
+  
+  @media (min-width: 769px) {
+    .container {
+      padding: 1%;
+    }
+  }
