a4a1b0d9978fa4b85c1a9ebe91f7e539b715d5d1
amexstedux <100746766+amexstedux@users.noreply.github.com>
2022-08-07 11:51:51 -0500

    h4
    h4

--- myscripts.js
+++ myscripts.js
@@ -1,25 +1,13 @@
 console.log ("Your index.js file is loaded correctly!");
-$( "#target" ).click(function() {
-    alert( "Handler for .click() called." );
-  });
-/*this is for the page up button */
-  mybutton = document.getElementById("myBtn");
-  window.onscroll = function() {scrollFunction()};
-  function scrollFunction() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-      mybutton.style.display = "block";
-    } else {
-      mybutton.style.display = "none";
-    }
-  } 
-  function topFunction() {
-    document.body.scrollTop = 0; // For Safari
-    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-  }
 
-  $(function() {
-    $("#myBtn2").on("click", function() {
-        $("body").animate({"scrollTop": window.scrollY-300}, 1000);
-        return false;
-    });
+$('pageDown').on('click', function(e){
+  if(this.hash !=='') {
+    e.preventDefault();
+
+    const hash = this.hash;
+
+    $('html, body').animate({
+      scrollTop: $(hash).offset().top
+    }, 800);
+  }
 });
